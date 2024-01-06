d445d7f5235331ed6529b4eaf6e446d12903876f
beefjerky <hwb0404@naver.com>
2024-01-06 20:21:37 +0900

    Initial commit

---
+++ src/main/java/com/example/preparingct/PreparingCtApplication.java
@@ -0,0 +1,13 @@
+package com.example.preparingct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreparingCtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreparingCtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/preparingct/PreparingCtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.preparingct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreparingCtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
